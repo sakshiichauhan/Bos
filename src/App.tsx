8d0ffe4e0d37fade6eb43eb1d2446c503ff0ba31
Karan Chourasia <kc2532002@gmail.com>
2025-06-27 15:18:52 +0530

    layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,25 @@
-import { Route, Routes } from "react-router-dom"
-import Landing from "./pages/Landing/Landing"
-import Register from "./pages/Register"
-import Partner from "./pages/Partner"
-import Sponser from "./pages/Sponser"
-import Join from "./pages/Join"
+import { Route, Routes } from "react-router-dom";
+import Landing from "./pages/Landing/Landing";
+import Register from "./pages/Register";
+import Partner from "./pages/Partner";
+import Sponser from "./pages/Sponser";
+import Join from "./pages/Join";
+import Layout from "./components/Layouts/Layout";
 
 const App = () => {
   return (
     <div>
-     <Routes>
-      <Route path="/" element={<Landing />} />
-      <Route path="/join" element={<Join />} />
-      <Route path="/partner" element={<Partner />} />
-      <Route path="/register" element={<Register />} />
-      <Route path="/sponser" element={<Sponser />} /> 
-     </Routes>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Landing />} />
+          <Route path="/join" element={<Join />} />
+          <Route path="/partner" element={<Partner />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/sponser" element={<Sponser />} />
+        </Routes>
+      </Layout>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
