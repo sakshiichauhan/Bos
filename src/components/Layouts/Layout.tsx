285f9ec483d7b4f8ae85424b7aa8d941669f461a
Karan Chourasia <kc2532002@gmail.com>
2025-06-27 15:10:51 +0530

    layout

--- src/components/Layouts/Layout.tsx
+++ src/components/Layouts/Layout.tsx
@@ -1,9 +1,10 @@
 import Footer from "./Footer";
 import Navbar from "./Navbar";
-export default function Layout() {
+export default function Layout({children}: {children: React.ReactNode}) {
   return (
     <div>
       <Navbar />
+      {children}
       <Footer />
     </div>
   )
