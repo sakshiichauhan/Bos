7bdd803e91775fc72fbc6ca4c93d9d1f2b9b72f8
sakshiichauhan <chauhan.sakshi2002@gmail.com>
2025-07-02 03:50:29 +0530

    Landing page

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import { Route, Routes } from "react-router-dom";
-import Landing from "@/Pages/Landing/Landing";
+import Landing from "./Pages/Landing/Landing";
 // import Register from "./pages/Register";
 // import Partner from "./pages/Partner";
-import Sponser from "@/Pages/Sponser";
-import Join from "@/Pages/Join";
-import Layout from "@/components/Layouts/Layout";
+import Sponser from "./Pages/Sponser";
+import Join from "./Pages/Join";
+import Layout from "./components/Layouts/Layout";
+
 // import PrivacyPolicies from "./pages/PrivacyPolicies";
 // import TermsAndConditions from "./pages/TermsConditions";
 // import NotFoundPage from "./pages/NotFoundPage";
